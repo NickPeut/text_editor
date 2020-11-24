92615b28a8edd4fb5890118660eb4408dd3ee6a9
NickPeut <46626621+NickPeut@users.noreply.github.com>
2020-11-24 21:27:46 +0300

    Create unique_sentence_in_text.h

---
+++ include/unique_sentence_in_text.h
@@ -0,0 +1,7 @@
+#ifndef UNIQUE_SENTENCE_IN_TEXT_H
+#define UNIQUE_SENTENCE_IN_TEXT_H
+
+    #include "text.h"
+    void printUniqueWords(Text_t* text, FILE* file);
+
+#endif //UNIQUE_SENTENCE_IN_TEXT_H

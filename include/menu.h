a27d174dd1e6601090c4e14ed93f63bf086961f5
NickPeut <46626621+NickPeut@users.noreply.github.com>
2020-11-24 21:24:57 +0300

    Create menu.h

---
+++ include/menu.h
@@ -0,0 +1,11 @@
+#ifndef MENU_H
+#define MENU_H
+
+    #include "text.h"
+    #include "unique_sentence_in_text.h"
+    #include "sort_sentence_product.h"
+    #include "update_format_date.h"
+    #include "processing_text.h"
+
+void menu(Text_t* text);
+#endif //MENU_H
